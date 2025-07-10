557e5c6e926b989c24637524d96be502fa2380a9
Platypus84 <mailtojulian@gmx.de>
2025-07-10 16:03:16 +0200

    solved_task_4.5.2.2

--- lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_2.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_2.dart
@@ -16,7 +16,15 @@ class MyStatelessWidget extends StatelessWidget {
     return Column(
       children: [
         Text('Hello Julian'),
-        ElevatedButton(onPressed: () => {}, child: Text('Klick mich'))
+        Padding(
+          padding: const EdgeInsets.only(top: 16.0),
+          child: ElevatedButton(
+            onPressed: () => {},
+            child: Text('Klick mich'),
+            style: ElevatedButton.styleFrom(
+                backgroundColor: Color.fromARGB(255, 255, 5, 209)),
+          ),
+        )
       ],
     );
   }
