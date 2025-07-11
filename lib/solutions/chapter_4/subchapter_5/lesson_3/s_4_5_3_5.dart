3bc844911bf5b5fd15d0bb8a45de92c5042b14ed
Platypus84 <mailtojulian@gmx.de>
2025-07-11 12:50:59 +0200

    solved_task_4.5.3.5

--- lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_5.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_5.dart
@@ -4,6 +4,35 @@ class S4535 extends StatelessWidget {
   const S4535({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return Column(
+      children: [
+        Row(children: [
+          Container(
+            height: 100,
+            width: 100,
+            color: Colors.red,
+          ),
+          Container(
+            height: 100,
+            width: 100,
+            color: Colors.green,
+          ),
+        ]),
+        Row(
+          children: [
+            Container(
+              height: 100,
+              width: 100,
+              color: Colors.blue,
+            ),
+            Container(
+              height: 100,
+              width: 100,
+              color: Colors.yellow,
+            ),
+          ],
+        ),
+      ],
+    );
   }
 }
