47906d467706f8681454c9388f8a27f9dba4ca30
Platypus84 <mailtojulian@gmx.de>
2025-07-14 15:02:57 +0200

    solved_task_4.5.3.6

--- lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_6.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_6.dart
@@ -2,8 +2,30 @@ import 'package:flutter/material.dart';
 
 class S4536 extends StatelessWidget {
   const S4536({super.key});
+
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    final List<Product> products = [
+      Product('Hemd', 19.99),
+      Product('Hose', 59.99),
+      Product('Pullover', 34.99),
+      Product('Socken', 4.99),
+      Product('T-Shirt', 9.99),
+    ];
+
+    return ListView.builder(
+        shrinkWrap: true,
+        itemCount: products.length,
+        itemBuilder: (context, index) => ListTile(
+            leading: Icon(Icons.shopping_cart),
+            title: Text(
+                'Produkt ${index + 1}: ${products[index].name}, ${products[index].price} €')));
+    //return ListView(children: [products[0],],)
   }
+}
+
+class Product {
+  String name;
+  double price;
+  Product(this.name, this.price);
 }
